061027d6742ceff8fb42937c2a55b1140ed76309
Yanco148 <58124757+Yanco148@users.noreply.github.com>
2021-09-14 07:35:56 +0200

    Update yt.py

--- yt.py
+++ yt.py
@@ -14,6 +14,8 @@ from telegram import InlineKeyboardButton , InlineKeyboardMarkup , InlineQueryRe
 
 ID_2 = int(os.environ["YOUR_ID"])
 
+YOUR_ID = int(os.environ["YOUR_ID"])
+
 TOKEN = os.environ["TOKEN"]
 
 def  alert (Update , context):
